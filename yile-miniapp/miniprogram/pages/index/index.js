2a0991b30b2a7c4a5d91982e16508d32ee9628ac
Your Name <you@example.com>
2026-02-10 18:51:20 +0800

    Update Mini Program URL to Vercel

--- yile-miniapp/miniprogram/pages/index/index.js
+++ yile-miniapp/miniprogram/pages/index/index.js
@@ -1,7 +1,7 @@
 Page({
     data: {
         // 默认链接，稍后您需要替换为 Vercel 的链接
-        url: 'http://localhost:3000'
+        url: 'https://yile-jiayuan.vercel.app/'
     },
     onLoad: function (options) {
         // 可以在这里接收参数
